82560bc38f7fe96dc11bac68a89539d547bed991
GE-Developer <on-line-misha@mail.ru>
2023-06-01 17:53:52 +0400

    Logo, colors, navBar, buttons added

---
+++ KazanSings/ViewControllers/BaseNavigationController.swift
@@ -0,0 +1,26 @@
+//
+//  BaseNavigationController.swift
+//  KazanSings
+//
+//  Created by M I C H A E L on 01.06.2023.
+//
+
+import UIKit
+
+final class BaseNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.backgroundColor = UIColor(named: "NavBarColor")
+        
+        navBarAppearance.titleTextAttributes = [
+            .foregroundColor: UIColor.white,
+            .font: UIFont.boldSystemFont(ofSize: 20)
+        ]
+        
+        navigationBar.scrollEdgeAppearance = navBarAppearance
+    }
+    
+}
---
+++ KazanSings/ViewControllers/StreamViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StreamViewController.swift
+//  KazanSings
+//
+//  Created by M I C H A E L on 01.06.2023.
+//
+
+import UIKit
+
+final class StreamViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Казань Поет"
+    }
+    
+}
