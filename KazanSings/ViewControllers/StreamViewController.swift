f7ec295fdf4f995a6d43cd817e1f018d93466b62
Ilnur Fattakhov <nurich1995@mail.ru>
2023-06-02 00:14:10 +0300

    add play / pause / timer / info buttons

--- KazanSings/ViewControllers/StreamViewController.swift
+++ KazanSings/ViewControllers/StreamViewController.swift
@@ -8,10 +8,39 @@
 import UIKit
 
 final class StreamViewController: UIViewController {
-
+    
+    var playSing = false
+    
+    @IBOutlet var playButton: UIButton!
+    @IBOutlet var infoButton: UIButton!
+    @IBOutlet var timerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Казань Поет"
+        title = "Казань Поёт"
+        playButton.setImage(UIImage(named: "playbutton.png"), for: .normal)
+        infoButton.setImage(UIImage(named: "infobutton"), for: .normal)
+        timerButton.setImage(UIImage(named: "timerbutton"), for: .normal)
     }
+    
+    @IBAction func playButtonTapped() {
+        // это чисто побаловаться, там потом нужно будет в mediaplayer менять кнопки
+        if !playSing {
+            playButton.setImage(UIImage(named: "pausebutton.png"), for: .normal)
+            playSing = true
+        } else {
+            playButton.setImage(UIImage(named: "playbutton.png"), for: .normal)
+            playSing = false
+        }
+    }
+    
+    @IBAction func timerButtonTapped() {
+        
+    }
+    
+    @IBAction func infoButtonTapped() {
+        
+    }
+    
     
 }
