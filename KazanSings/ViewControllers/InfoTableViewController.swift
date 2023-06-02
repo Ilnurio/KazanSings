8afc81f4c72bbcb1bcec2c4ee70d8b221fbf2934
GE-Developer <on-line-misha@mail.ru>
2023-06-03 01:03:53 +0400

    /

--- KazanSings/ViewControllers/InfoTableViewController.swift
+++ KazanSings/ViewControllers/InfoTableViewController.swift
@@ -17,20 +17,17 @@ final class InfoTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
-//
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return info.count
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
         var content = cell.defaultContentConfiguration()
     
         content.text = info[indexPath.row].title
+        content.textProperties.color = .white
         content.image = UIImage(systemName: info[indexPath.row].imageName)
         
         cell.contentConfiguration = content
