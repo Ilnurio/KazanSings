95a7095076a3e3cea71fa5352d17e2e8fe990c35
GE-Developer <on-line-misha@mail.ru>
2023-06-03 05:31:14 +0400

    Radio Started

--- KazanSings/ViewControllers/StreamViewController.swift
+++ KazanSings/ViewControllers/StreamViewController.swift
@@ -6,45 +6,79 @@
 //
 
 import UIKit
+import MediaPlayer
 
 final class StreamViewController: UIViewController {
-//
-//    var playSing = false
-//
-//    @IBOutlet var playButton: UIButton!
-//    @IBOutlet var infoButton: UIButton!
-//    @IBOutlet var timerButton: UIButton!
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        title = "Казань Поёт"
-//        playButton.setImage(UIImage(named: "playbutton.png"), for: .normal)
-//        infoButton.setImage(UIImage(named: "infobutton.png"), for: .normal)
-//        timerButton.setImage(UIImage(named: "timerbutton.png"), for: .normal)
-//    }
-//
-//    @IBAction func playButtonTapped() {
-//        // это чисто побаловаться, там потом нужно будет в mediaplayer менять кнопки
-//        if !playSing {
-//            playButton.setImage(UIImage(named: "pausebutton.png"), for: .normal)
-//            playSing = true
-//        } else {
-//            playButton.setImage(UIImage(named: "playbutton.png"), for: .normal)
-//            playSing = false
-//        }
-//    }
-//
-//
-//    @IBAction func shareSheetTapped(_ sender: UIBarButtonItem) {
-//        presentShareSheet()
-//    }
-//
-//    @IBAction func timerButtonTapped() {
-//
-//    }
-//}
-//
-// // MARK: - ShareSheet
+    
+    @IBOutlet private var playButton: UIButton!
+    
+    let commandCenter = MPRemoteCommandCenter.shared()
+    private var player: AVPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        player = AVPlayer(url: URL(string: "https://stream01.hitv.ru:8443/kazansings-320kb")!)
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            //try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print("Ошибка настройки аудиосессии: \(error.localizedDescription)")
+        }
+        
+        commandCenter.playCommand.addTarget { [weak self] _ in
+            self?.player.play()
+            return .success
+        }
+        
+        // Добавляем обработчик для команды паузы
+        commandCenter.pauseCommand.addTarget { [weak self] _ in
+            self?.player.pause()
+            return .success
+        }
+    
+    }
+    
+    @IBAction private func playButtonPressed() {
+        if player.timeControlStatus == .playing {
+//            try! AVAudioSession.sharedInstance().setActive(false)
+            player.pause()
+            playButton.setImage(UIImage(named: "playbutton"), for: .normal)
+        } else {
+//            try! AVAudioSession.sharedInstance().setActive(true)
+            player.play()
+            playButton.setImage(UIImage(named: "pausebutton"), for: .normal)
+        }
+        
+        updateNowPlayingInfo()
+    }
+    
+    private func setupPlayButton() {
+        if player.timeControlStatus == .playing {
+            playButton.setBackgroundImage(UIImage(named: "playbutton"), for: .normal)
+        } else {
+            playButton.setBackgroundImage(nil, for: .normal)
+            playButton.setBackgroundImage(UIImage(named: "pausebutton"), for: .normal)
+        }
+    }
+    
+    private func updateNowPlayingInfo() {
+        var nowPlayingInfo = [String: Any]()
+        nowPlayingInfo[MPMediaItemPropertyTitle] = "Казань поет"
+        nowPlayingInfo[MPMediaItemPropertyArtist] = "Радио - Прямой эфир"
+        
+        MPNowPlayingInfoCenter.default().nowPlayingInfo = nowPlayingInfo
+        
+        if let artworkImage = UIImage(named: "AppIcon") {
+            let artwork = MPMediaItemArtwork(boundsSize: artworkImage.size) { _ in
+                return artworkImage
+            }
+            MPNowPlayingInfoCenter.default().nowPlayingInfo?[MPMediaItemPropertyArtwork] = artwork
+        }
+    }
+}
+
+ // MARK: - ShareSheet
 //extension StreamViewController {
 //    func presentShareSheet() {
 //        let image = UIImage(named: "logosheet.jpg")!
@@ -53,4 +87,4 @@ final class StreamViewController: UIViewController {
 //        shareSheet.popoverPresentationController?.sourceView = self.view
 //        present(shareSheet, animated: true, completion: nil)
 //    }
-}
+//}
