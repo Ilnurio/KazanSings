0a7e172c6d948c46dfc8cf5cc1bb1d60dde1fa83
GE-Developer <on-line-misha@mail.ru>
2023-06-06 01:30:37 +0400

    /

--- KazanSings/Models/AboutUs.swift
+++ KazanSings/Models/AboutUs.swift
@@ -5,28 +5,40 @@
 //  Created by Ilnur on 05.06.2023.
 //
 
-import Foundation
+enum LinkName {
+    case vk
+    case telegram
+    case github
+}
 
 struct AboutUs {
     let imageName: String
     let nameSurName: String
     let statusInTeam: String
+    let link: [LinkName: String]
+    
+    var description: String?
 }
 
 extension AboutUs {
     static func getAboutUs() -> [AboutUs] {
-        [AboutUs(
+        [
+            AboutUs(
             imageName: "teamlead",
             nameSurName: "Андрей Лысяков",
-            statusInTeam: "Tимлид"),
+            statusInTeam: "Tимлид",
+            link: [.vk: "", .telegram: ""],
+            description: ""),
          AboutUs(
             imageName: "programmerone",
-            nameSurName: "Михаил Батуми",
-            statusInTeam: "IOS Разработчик"),
+            nameSurName: "Михаил Бухрашвили",
+            statusInTeam: "IOS Разработчик",
+            link: [.github: "https://github.com/GE-Developer"]),
          AboutUs(
             imageName: "programmertwo",
             nameSurName: "Ильнур Фаттахов",
-            statusInTeam: "IOS Разработчик")
+            statusInTeam: "IOS Разработчик",
+            link: [.github: ""])
         ]
     }
 }
