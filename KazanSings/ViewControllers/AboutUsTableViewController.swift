ec4f1ec5c255dfc867530c03858493f4638cbf2c
Ilnur Fattakhov <nurich1995@mail.ru>
2023-06-06 12:00:51 +0300

    add properties for text and secondaryText

--- KazanSings/ViewControllers/AboutUsTableViewController.swift
+++ KazanSings/ViewControllers/AboutUsTableViewController.swift
@@ -25,13 +25,19 @@ final class AboutUsTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "personCell", for: indexPath)
         var content = cell.defaultContentConfiguration()
         let aboutUs = aboutUs[indexPath.row]
+        
         content.text = aboutUs.nameSurName
         content.secondaryText = aboutUs.statusInTeam
         content.textProperties.color = .white
+        content.textProperties.font = .boldSystemFont(ofSize: 17)
+        content.secondaryTextProperties.color = .white
+        content.secondaryTextProperties.font = .systemFont(ofSize: 15)
+        
         content.image = UIImage(named: aboutUs.imageName)
         content.imageProperties.cornerRadius = tableView.rowHeight / 2
         content.imageProperties.tintColor = .white
         cell.separatorInset.left = view.frame.width
+        
         cell.contentConfiguration = content
         
         return cell
