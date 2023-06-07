047b4912678cd3b46138e99dbbc0b7d2fd83ea89
Ilnur Fattakhov <nurich1995@mail.ru>
2023-06-07 15:03:56 +0300

    / some bugs appear

---
+++ KazanSings/ViewControllers/FounderViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FounderViewController.swift
+//  KazanSings
+//
+//  Created by Ilnur on 07.06.2023.
+//
+
+import UIKit
+
+class FounderViewController: UIViewController {
+    
+    @IBOutlet var imageLabel: UIImageView!
+    @IBOutlet var introducationLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    private let aboutUs = AboutUs.getAboutUs()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.imageLabel.round(
+            cornerRadius: self.imageLabel.frame.size.width / 2,
+            borderWidth: 3.0,
+            borderColor: .white
+        )
+        
+        introducationLabel.text = aboutUs
+    }
+}
+
+extension UIView {
+    func round(cornerRadius: CGFloat, borderWidth: CGFloat, borderColor: UIColor) {
+        
+        self.layer.cornerRadius = cornerRadius
+        self.layer.borderWidth = borderWidth
+        self.layer.borderColor = borderColor.cgColor
+        self.clipsToBounds = true
+    }
+}
